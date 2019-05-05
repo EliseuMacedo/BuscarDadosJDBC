c85dfe6382859a7a48f73ee2034451f2df7cfce3
Eliseu Macedo <eliseumcd@gmail.com>
2019-05-04 23:14:12 -0300

    Exemplo concluído

---
+++ src/application/Program.java
@@ -0,0 +1,47 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import db.DB;
+
+
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Connection conn = null;
+		
+		Statement st = null;
+		
+		ResultSet rs = null;
+		
+		try {
+			//conectar o bd
+			conn = DB.getConnection();
+			//instanciar objeto do tipo statement
+			st = conn.createStatement();
+			
+			
+			//metodo execute query e o resultado da consulta será atribuido na variável rs. resultset
+			rs =  st.executeQuery("select * from department");
+			
+			while(rs.next()) {
+				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+			}
+		}
+		
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+	}
+
+}
